afa86418ae446ac6e5083a3545089f5e0084ff17
(s) Adam Viant <adam.viant@students.plymouth.ac.uk>
2020-01-07 19:36:16 +0000

    ammended addData.js

--- Server/addData.js
+++ Server/addData.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://Test:<password>@cluster-l4srv.mongodb.net/test?retryWrites=true&w=majority",
 { useNewUrlParser: true });
 
+var Category = mongoose.model("Category", {id: Number, userID: Number, categoryName: String, status: Boolean});
 var testData = new Category ({id: 1, userID: 1, categoryName: Soft355, status: 0})
 
 testData.save(function(err) {
