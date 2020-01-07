96660d8516ccdc4f6ce11bcd8b187727f6d1b6b6
(s) Adam Viant <adam.viant@students.plymouth.ac.uk>
2020-01-07 20:40:03 +0000

    Ammended the Mongodb link to connect to the correct DB

--- Server/addData.js
+++ Server/addData.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://TimeMangement:time@cluster-l4srv.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true });
+mongoose.connect("mongodb+srv://TimeMangement:time@cluster-l4srv.mongodb.net/timeManagementDB?retryWrites=true&w=majority", {useNewUrlParser: true });
 var category = require("./catagory");
 
-var testData = new category ({id: 2, userID: 3, categoryName: "Soft355", status: 0})
+var testData = new category ({id: 1, userID: 1, categoryName: "Soft355", status: 0})
 
 testData.save(function(err) {
   console.log("Saved Test Data");
