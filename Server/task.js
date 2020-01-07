b599cfa78d45f902cc1884d8cfcf6c7f7df3e29a
(s) Adam Viant <adam.viant@students.plymouth.ac.uk>
2020-01-07 20:40:40 +0000

    removed a #

--- Server/task.js
+++ Server/task.js
@@ -2,4 +2,4 @@ var mongoose = require("mongoose");
 var schema = mongoose.Schema;
 
 var taskSchemas = new schema ({id: Number, categoryID: Number, taskName: String, estimatedHours: Number, totalHours: Number, status: Boolean});
-#module.exports = mongoose.model("task", taskSchemas);
+module.exports = mongoose.model("task", taskSchemas);
