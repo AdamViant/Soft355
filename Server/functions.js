6c4dec316435d360a419d5b98b45e6a4a6742c0f
(s) Adam Viant <adam.viant@students.plymouth.ac.uk>
2020-01-08 17:23:57 +0000

    Created add functions

--- Server/functions.js
+++ Server/functions.js
@@ -1,6 +1,32 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://TimeMangement:time@cluster-l4srv.mongodb.net/timeManagementDB?retryWrites=true&w=majority";
 
+function addCategory(id, uid, name, status) {
+  MongoClient.connect(url, {useNewUrlParser: true},function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("timeManagementDB");
+    var myobj = {id: id, userID: uid, categoryName: name, status: status};
+    dbo.collection("categorys").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+}
+module.exports.addCategory = addCategory;
+
+function addTask(id, catID, name, eHours, tHours, status)  {
+  MongoClient.connect(url, {useNewUrlParser: true},function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("timeManagementDB");
+    var myobj = {id: id, categoryID: catId, taskName: name, estimatedHours: eHours, totalHours: tHours, status: status};
+    dbo.collection("tasks").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+}
+module.exports.addTask = addTask;
+
 function findCatagorys() {
   MongoClient.connect(url, {useNewUrlParser: true}, function(err, db) {
   if (err) throw err;
@@ -12,5 +38,4 @@ function findCatagorys() {
   });
 });
 }
-
 module.exports.findCatagorys = findCatagorys;
