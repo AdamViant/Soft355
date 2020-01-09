85374e518329fe8bd35965eddad9ce93075431dd
(s) Adam Viant <adam.viant@students.plymouth.ac.uk>
2020-01-09 22:12:51 +0000

    Ammending index.html to display data in a nice manner.

--- Server/Site/script.js
+++ Server/Site/script.js
@@ -1,7 +1,9 @@
 
 var app = angular.module('myApp', []);
 app.controller("myCtrl", function($scope, $http) {
+  $scope.Array = [];
   $http.get("/index").then(function(response) {
     console.log(response);
+    $scope.Array = response.data;
   })
 })
